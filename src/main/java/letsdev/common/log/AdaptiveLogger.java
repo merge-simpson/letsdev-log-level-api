7edaccedc7e956a455df4d8327f475214015eddc
merge-simpson <letsdev@kakao.com>
2025-04-16 10:47:06 +0900

    fix(log): remove all caches from `AdaptiveLogger`

--- src/main/java/letsdev/common/log/AdaptiveLogger.java
+++ src/main/java/letsdev/common/log/AdaptiveLogger.java
@@ -4,13 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 
-import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class AdaptiveLogger {
     private final Logger logger;
-    private final Map<LogLevel, LevelFixedLogger> cachedLoggers = new ConcurrentHashMap<>();
 
     protected <T> AdaptiveLogger(Class<T> targetClass) {
         this(targetClass.getName());
@@ -27,10 +24,9 @@ public class AdaptiveLogger {
     }
 
     public static <T> AdaptiveLogger getLogger(String name) {
+        // NOTE String::isBlank is since JDK 11
         final String sourceName = isBlank(name) ? "unnamed" : name;
-        return AdaptiveLoggerHolder.ADAPTIVE_LOGGER_MAP.computeIfAbsent(
-                sourceName, (ignore) -> new AdaptiveLogger(sourceName)
-        );
+        return new AdaptiveLogger(sourceName);
     }
 
     private static boolean isBlank(String str) {
@@ -49,18 +45,11 @@ public class AdaptiveLogger {
 
     public LevelFixedLogger with(LogLevel logLevel) {
         Objects.requireNonNull(logLevel);
-        return cachedLoggers.computeIfAbsent(
-                logLevel,
-                (level) -> new LevelFixedLogger(logger, level)
-        );
+        return new LevelFixedLogger(logger, logLevel);
     }
 
     public LevelFixedLogger with(Level logLevel) {
         Objects.requireNonNull(logLevel);
         return with(LogLevel.valueOf(logLevel.name()));
-    }
-
-    private static class AdaptiveLoggerHolder {
-        private static final Map<String, AdaptiveLogger> ADAPTIVE_LOGGER_MAP = new ConcurrentHashMap<>();
     }
 }
